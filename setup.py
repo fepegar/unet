e3f2f3bcecacf4c0eec0dc21c93f37f4b5ee89b4
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-07-16 13:52:03 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='unet',
+    version='0.1.0',
+    author='Fernando Perez-Garcia',
+    author_email='fernando.perezgarcia.17@ucl.ac.uk',
+    description='PyTorch implementation of UNet',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fepegar/unet',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+    ],
+)
