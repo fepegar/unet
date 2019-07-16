cc7cda1f7fcc110379b377d1c0abcd3472fd1417
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-07-16 14:11:19 +0100

    Add __init__

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='unet',
-    version='0.1.0',
+    version='0.1.1',
     author='Fernando Perez-Garcia',
     author_email='fernando.perezgarcia.17@ucl.ac.uk',
     description='PyTorch implementation of UNet',
