7fb50c81c517c95e8dd6eed59be13da6723c29c2
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-11-04 10:33:08 +0000

    Bump version: 0.7.1 → 0.7.2

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/unet',
-    version='0.7.1',
+    version='0.7.2',
     zip_safe=False,
 )
