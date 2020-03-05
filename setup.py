9f64483d351b4f7d95c0d871aa7aa587b8fdb21b
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2020-03-05 13:20:37 +0000

    Bump version: 0.7.4 → 0.7.5

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fepegar/unet',
-    version='0.7.4',
+    version='0.7.5',
     zip_safe=False,
 )
--- unet/__init__.py
+++ unet/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.7.4'
+__version__ = '0.7.5'
 
 from .unet import UNet, UNet2D, UNet3D
